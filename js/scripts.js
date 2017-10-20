15a941b95ef8d12e76fa6ea71f75400c2f2716dc
Glenne <glenne.savage@cox.net>
2017-10-20 13:17:01 -0700

    Added smooth scrolling and menu animation on scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,57 @@
-//Mobile Menu Animation
+// Shrink-Scroll Header
+shrinkScroll();
+
+function shrinkScroll() {
+    // Variables to grab elements
+    var header = document.querySelector('header');
+    var logo = document.querySelector('header h1 a');
+    var minMenu = document.querySelector('#menu-toggle');
+    
+    // Listen for page scroll and animate
+    window.addEventListener('scroll', function (){
+        if (window.pageYOffset > 80) {
+            header.classList += ' small-head';
+            logo.classList += ' small-logo';
+            minMenu.style.top = '22%';
+        } else {
+            header.classList = '';
+            logo.classList = '';
+            minMenu.style.top = '';
+        }
+    });
+}
+
+// Jquery Scrolling
+$(document).ready(function () {
+    var scrollLink = $('.scroll');
+
+    // Smooth Scrolling
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body, html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 750);
+    });
+
+    // Active Link Switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+            var sectionOffset = $(this.hash).offset().top -65;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+
+});
+
+
+
+// Mobile Menu Animation
 mobMenu();
 
 function mobMenu() {
@@ -24,28 +77,6 @@ function mobMenu() {
     }
 }
 
-// Shrink-Scroll Header
-shrinkScroll();
-
-function shrinkScroll() {
-    // Variables to grab elements
-    var header = document.querySelector('header');
-    var logo = document.querySelector('header h1 a');
-    var minMenu = document.querySelector('#menu-toggle');
-    
-    // Listen for page scroll and animate
-    window.addEventListener('scroll', function (){
-        if (window.pageYOffset > 80) {
-            header.classList += ' small-head';
-            logo.classList += ' small-logo';
-            minMenu.style.top = '22%';
-        } else {
-            header.classList = '';
-            logo.classList = '';
-            minMenu.style.top = '';
-        }
-    });
-}
 
 // Footer Copyright Date
 var year = new Date().getFullYear();
