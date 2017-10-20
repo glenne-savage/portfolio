7767143b7fb01213826131cb04ff7bb0052d5b96
Glenne <glenne.savage@cox.net>
2017-10-19 20:47:43 -0700

    Added mobile media queris and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,51 @@
-// Shrink-Scroll Header
-var header = document.querySelector('header');
-var logo = document.querySelector('header h1 a');
+//Mobile Menu Animation
+mobMenu();
 
-window.addEventListener('scroll', function (){
-    if (window.pageYOffset > 80) {
-        header.classList += ' small-head';
-        logo.classList += ' small-logo';
-    } else {
-        header.classList = '';
-        logo.classList = '';
+function mobMenu() {
+    // Variables to grab elements
+    var menuToggle = document.querySelector('#menu-toggle');
+    var navUl = document.querySelector('.flex-nav ul');
+    var navLi = document.querySelectorAll('.flex-nav ul li');
+   
+    // Listen for click on menu
+    menuToggle.addEventListener('click', openCloseMenu(), false);
+    
+    // Loop through menu list and listen for click to close menu
+    for (var i = 0; i < navLi.length; i++) {
+        navLi[i].addEventListener('click', openCloseMenu(), false);
     }
-});
+
+    // Animate mobile menu toggle
+    function openCloseMenu() {
+        return function() {
+            menuToggle.classList.toggle('menu-toggle-open');
+            navUl.classList.toggle('open');
+        };
+    }
+}
+
+// Shrink-Scroll Header
+shrinkScroll();
+
+function shrinkScroll() {
+    // Variables to grab elements
+    var header = document.querySelector('header');
+    var logo = document.querySelector('header h1 a');
+    var minMenu = document.querySelector('#menu-toggle');
+    
+    // Listen for page scroll and animate
+    window.addEventListener('scroll', function (){
+        if (window.pageYOffset > 80) {
+            header.classList += ' small-head';
+            logo.classList += ' small-logo';
+            minMenu.style.top = '22%';
+        } else {
+            header.classList = '';
+            logo.classList = '';
+            minMenu.style.top = '';
+        }
+    });
+}
 
 // Footer Copyright Date
 var year = new Date().getFullYear();
